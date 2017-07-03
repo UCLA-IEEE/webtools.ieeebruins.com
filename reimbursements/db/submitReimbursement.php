0b71eb7f3e239e50a0c61123da653f2ddd286ffc
Jeffrey Chan <jeffschan97@gmail.com>
2017-07-03 09:57:52 -0700

    More basic structure. Created new datbase module.

--- reimbursements/db/submitReimbursement.php
+++ reimbursements/db/submitReimbursement.php
@@ -3,7 +3,7 @@
 $dbUsername = file_get_contents('./dbUsername.txt');
 $dbPassword = file_get_contents('./dbPassword.txt');
 
-echo $dbUsername;
-echo $dbPassword;
+// mysql_connect('localhost', $dbUsername, $dbPassword);
+// mysql_select_db('ieeebrui_tools');
 
 ?>
--- reimbursements/index.php
+++ reimbursements/index.php
@@ -12,65 +12,62 @@
     <!-- BOOTSTRAP -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <!-- STYLESHEETS -->
+    <link rel='stylesheet' href='./reimbursements/public/style.css' />
+    <link rel='stylesheet' href='./reimbursements/public/form.css' />
 </head>
 <body>
 
     <!-- NAVBAR -->
     <?php include('../views/includes/navbar.html'); ?>
 
-    <!-- BANNER -->
-    <div class="container text-center">
-        <h2>Welcome to Reimbursements</h2>
-    </div>
+    <!-- REIMBURSEMENT FORM -->
+    <div class="form-wrapper">
+        <h1 class="text-center">Reimbursement Form</h1>
 
-    <!-- REIMBURSEMTN FORM -->
-    <div class="container">
-        <h1 class="col-md-6 col-md-offset-3 text-center">Reimbursement Form</h1>
+        <form enctype="multipart/form-data" action="./reimbursements/db/submitReimbursement.php" method="POST" class="form">
+            <!-- NAME -->
+            <div class="form-group">
+                <label for="name" class="control-label">Name</label>
+                <input type="text" class="form-control" name="name" id="name">
+            </div>
 
-        <div class="form col-md-6 col-md-offset-3">
-            <form enctype="multipart/form-data" action="./reimbursements/db/submitReimbursement.php" method="POST" class="form">
-                <!-- NAME -->
-                <div class="form-group">
-                    <label for="name" class="control-label">Name</label>
-                    <input type="text" class="form-control" name="name" id="name">
+            <!-- CONTACT INFO -->
+            <div class="form-group">
+                <label for="contact" class="control-label">Contact Phone # / Email / Venmo</label>
+                <input class="form-control" type="text" name="contact" id="contact">
+            </div>
+
+            <!-- REASON -->
+            <div class="form-group">
+                <label for="reason" class="control-label">Reason for reimbursement?</label>
+                <textarea class="form-control" id="reason" rows="5" cols="40" name="reason"></textarea>
+            </div>
+
+            <!-- REIMBURSEMENT AMOUNT -->
+            <div class="form-group">
+                <label for="amount" class="control-label">Amount to be reimbursed</label>
+                <div class="input-group">
+                    <span class="input-group-addon">$</span>
+                    <input class="form-control" type="text" name="amount" id="amount">
                 </div>
+            </div>
 
-                <!-- CONTACT INFO -->
-                <div class="form-group">
-                    <label for="contact" class="control-label">Contact Phone # / Email / Venmo</label>
-                    <input class="form-control" type="text" name="contact" id="contact">
-                </div>
+            <!-- RECEIPT -->
+            <div class="form-group">
+                <input type="hidden" name="MAX_FILE_SIZE" value="20000000"/>
+                <label for="file" class="control-label">
+                    Upload a receipt<br/>(2MB size limit. JPEG/PDF only)
+                </label>
+                <input class="form-control" name="receipt" type="file" id="file"/>
+            </div>
 
-                <!-- REASON -->
-                <div class="form-group">
-                    <label for="reason" class="control-label">Reason for reimbursement?</label>
-                    <textarea class="form-control" id="reason" rows="5" cols="40" name="reason"></textarea>
-                </div>
-
-                <!-- REIMBURSEMENT AMOUNT -->
-                <div class="form-group">
-                    <label for="amount" class="control-label">Amount to be reimbursed</label>
-                    <div class="input-group">
-                        <span class="input-group-addon">$</span>
-                        <input class="form-control" type="text" name="amount" id="amount">
-                    </div>
-                </div>
-
-                <!-- RECEIPT -->
-                <div class="form-group">
-                    <input type="hidden" name="MAX_FILE_SIZE" value="20000000"/>
-                    <label for="file" class="control-label">
-                        Upload a receipt<br/>(2MB size limit. JPEG/PDF only)
-                    </label>
-                    <input class="form-control" name="receipt" type="file" id="file"/>
-                </div>
-
-                <!-- SUBMIT -->
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary form-control">Submit</button>
-                </div>
-            </form>
-        </div>
+            <!-- SUBMIT -->
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary form-control">Submit</button>
+            </div>
+        </form>
     </div>
 
     <!-- FOOTER -->
